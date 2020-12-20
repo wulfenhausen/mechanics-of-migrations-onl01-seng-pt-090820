8b586712c7195b8fa58077bfb118758d30104903
William Han Findiesen <william.findiesen@gmail.com>
2020-12-20 21:19:09 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,11 +1,9 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
   
   def up
-    
   end
   
   def down
-    
   end
   
   def change #the primary way of writing migrations. It works for the majority of cases where ActiveRecord knows how to reverse the migration automatically
